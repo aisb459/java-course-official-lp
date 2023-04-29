5ba79e9202e1f9a3844ec2672f5afe4b74986cd4
aisb459 <85751152+aisb459@users.noreply.github.com>
2023-04-29 14:55:24 +0530

    Add files via upload

---
+++ spring-boot-more-on-restapi/src/main/java/com/herbalife/springbootmoreonrestapi/SpringBootMoreOnRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.herbalife.springbootmoreonrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMoreOnRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMoreOnRestapiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-more-on-restapi/src/test/java/com/herbalife/springbootmoreonrestapi/SpringBootMoreOnRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbalife.springbootmoreonrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMoreOnRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
